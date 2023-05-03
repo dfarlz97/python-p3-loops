074e98f8b25a62a420ab5fae8c4b5ca0f7b46235
+ <+>
2023-05-03 11:01:35 -0400

    satisfy deliverables

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while (i >= 1):
+        print(f"{i}")
+        i -= 1
+        if i == 0:
+            print("Happy New Year!")
+        
+
+square_nums = list()
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    square_list = [num * num for num in int_list]
+    square_nums.append(square_list)
+    return square_list 
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(101):
+        if num % 3 == 0 and num % 5 == 0 and num != 0:
+            print("FizzBuzz")
+        elif num % 5 == 0 and num != 0:
+            print("Buzz")
+        elif num % 3 == 0 and num != 0:
+            print("Fizz")
+        else:
+            if num != 0:
+                print(num)
+
+
